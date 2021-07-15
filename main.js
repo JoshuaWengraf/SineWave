d769423d464c20f4088115698127a93fd08c02fc
JoshuaWengraf <64872799+JoshuaWengraf@users.noreply.github.com>
2021-07-15 17:36:44 +0100

    Added Gui

--- main.js
+++ main.js
@@ -1,75 +1,58 @@
 const blue = "#0000ff"
-let drawCircle = function (x, y, radius) {
-	context.beginPath();
-	context.fillStyle = blue;
-	context.arc(x, y, radius, 0, Math.PI * 2, false);
-	context.closePath();
-	context.fill();
-}
 
-let drawAxis = function (){
-    context.beginPath();
-    context.lineWidth = 10;
-	context.moveTo(0,0)
-    for (let x = 0; x < canvas.width; x++){
-        context.lineTo(x, canvas.height)
-    }
-    context.stroke()
-} 
-
-
-let drawSineWave = function (offset, amplitude, frequency){
-    context.beginPath()
-    context.moveTo(0, canvas.height / 2)
-
-    for (let x = 0; x < canvas.width; x++){
-        let y = canvas.height/2 + 100 * Math.sin(x * 0.01) 
-        context.lineTo(x, y);
-    }
-
-    context.stroke()
-} 
-
-let drawWindow = function() {
-	context.clearRect(0,0,canvas.width,canvas.height);
-}
+const gui = new dat.GUI()
 
 let canvas = document.getElementById("canvas");
 context = canvas.getContext('2d');
 
-let x = 0;
-let ballRadius = 20
-let offset = canvas.height/2
-let amplitude = 100
-let frequency = 0.01
+const wave = {
+    y: canvas.height / 2,
+    amplitude: 10,
+    frequency: 0.01,
 
-function draw(){
+    draw() {
+        context.clearRect(0,0,canvas.width, canvas.height)
+        context.beginPath()
+        context.moveTo(0, canvas.height / 2)
 
-    document.getElementById("myRange").value = x
-    xMouse = document.getElementById("myRange").value;
+        for (let x = 0; x < canvas.width; x++){
+            let y = wave.y + wave.amplitude * Math.sin(x * wave.frequency) 
+            context.lineTo(x, y );
+        }
+        context.stroke()
+    }
+}
 
-	drawWindow();
-    drawSineWave(offset, amplitude, frequency);
-    drawAxis()
 
+const ball = {
+    x: 0,
+    y:  wave.y + wave.amplitude * Math.sin(this.x * wave.frequency),
+    radius: 10,
+
+    draw(x,y, radius){
+        context.beginPath();
+        context.fillStyle = blue;
+        context.arc(x, y, radius, 0, Math.PI * 2, false);
+        context.closePath();
+        context.fill();
+    }
+}
+
+
+gui.add(wave, 'y', 0, canvas.height)
+gui.add(wave, 'amplitude', -300, 300)
+gui.add(wave, 'frequency', 0,1)
+
+gui.add(ball, 'x', 0, canvas.width)
+
+
+function animate() {
+    requestAnimationFrame(animate)
     
+    wave.draw()
+    ball.draw()
 
-    y = offset + amplitude*Math.sin(x * frequency) 
-
-    drawCircle(xMouse,y, ballRadius);
-
-    x = x + 1
-
-    if (x > canvas.width){
-        x = 0
-    }  
-
-   
-};
-
-
-setInterval(draw,10); 
-
-
-
-
+    let y = wave.y + wave.amplitude * Math.sin(ball.x * wave.frequency) 
+    ball.draw(ball.x, y, ball.radius)
+}
+animate()
